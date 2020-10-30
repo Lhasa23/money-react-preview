adeb7a73fd3233952bad368e4dbcd02e345cab50
eleven-shi <sydryy@gmail.com>
2020-10-31 02:29:04 +0800

    init

---
+++ precache-manifest.5f17e4348266e31ef58ae30e2b7787a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15edeb6359b0cfcb4f688fd50eb21a67",
+    "url": "/money-react-preview/index.html"
+  },
+  {
+    "revision": "b6da7c58fe63fd91fa23",
+    "url": "/money-react-preview/static/css/main.07cd035a.chunk.css"
+  },
+  {
+    "revision": "77e4a19db02a0696594a",
+    "url": "/money-react-preview/static/js/2.0ce736ab.chunk.js"
+  },
+  {
+    "revision": "4648a5ac342ec5040769b374fc01c4fd",
+    "url": "/money-react-preview/static/js/2.0ce736ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6da7c58fe63fd91fa23",
+    "url": "/money-react-preview/static/js/main.4e44949f.chunk.js"
+  },
+  {
+    "revision": "84c89f27db89ea7e506b",
+    "url": "/money-react-preview/static/js/runtime-main.44e02bdd.js"
+  }
+]);
